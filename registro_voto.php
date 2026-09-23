4a942e7f021919756e886123ae583c9bd37ad666
Nicolas Moreno <est.moreno.nicolas@latecnicalf.com.ar>
2026-09-23 18:02:17 -0300

    registro voto a php

--- registro_voto.php
+++ registro_voto.php
@@ -1,3 +1,4 @@
+
 <?php
 $voto = $_POST["voto"];
 
@@ -8,3 +9,24 @@ if ($voto == "cacique1")
         //$v
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voto Realizado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1 class="VTitulo">Voto realizado</h1>
+        <p class="VDesc">Tu voto ha sido enviado. Gracias por participar!</p>
+    </main>
+    
+    <section class="VBoton">
+        <div>
+            <button class="Volver">Registro</button>
+        </div>
+    </section>
+</body>
+</html>
