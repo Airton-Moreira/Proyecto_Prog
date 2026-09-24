69c52c3f94381ae72e795678f1038f425998dd6b
Airton Moreira <est.moreira.itzel@latecnicalf.com.ar>
2026-09-24 13:44:20 -0300

    js push

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ let total = 0;
 
 // la podria declarar con id 
 // lo voy a poder sumas por cada boton
-const boton = document.getElementById("Boton");
+const botones = document.querySelectorAll(".BotonVotar");
 const resultado = document.getElementById("Votos");
 
- boton.addEventListener("click", function() {
-     total += 1; 
-     //con textcontent 
-     resultado.textContent = total;  
-    });
-   
+botones.forEach(function(boton) {
 
-    
+boton.addEventListener("click", function() {
+
+const tarjeta = boton.parentElement;
+const resultado = tarjeta.querySelector(".Votos");
+
+let votos = Number(resultado.textContent);
+
+votos += 1;
+
+resultado.textContent = votos;
+
+            });
+
+        });
